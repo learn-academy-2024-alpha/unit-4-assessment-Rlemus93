e48ab8b9f7639b83ecd768eb94d6ceee46fac1ac
Ryan <rlemus93@gmail.com>
2024-03-17 16:35:59 -0700

    completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,29 +10,81 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
+
+// Template
+// describe("functionName", () => {
+//     it("what does the function do?", () => {
+//         expect(functionName()).toEqual(output)
+//     })
+// })
+
 // --------------------1) Prompt: Create a function that takes in a number (greater than 2) and returns an array containing the Fibonacci sequence. The length of the array is determined by the argument of the function.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibonacciLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+describe("fibonacciSequence", () => {
+  it("takes in a number and returns array containing the fibonacci sequence. array length is determined by the arg number", () => {
+    const fibonacciLength1 = 6
+    const fibonacciLength2 = 10
+    expect(fibonacciSequence(fibonacciLength1)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(fibonacciSequence(fibonacciLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  })
+}) 
+// ReferenceError: fibonacciSequence is not defined
 
-const fibonacciLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+const fibonacciSequence = (length) => {
+    if (length <= 2) {
+      return('Length must be greater than 2');
+    }
+  
+    const sequence = [1, 1];
+  
+    for (let i = 2; i < length; i++) {
+      sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+  
+    return sequence;
+  };
+
+// Pseudo code: 
+//created a function. takes in paramenter called length.
+//started conditional where if length is less then or = to 0 to return a string. since fibonacci sequence typically requires a length greater than 2.
+//created variable sequence that is = [1,1] the starting array.
+//within the loop, starting from i = 2, each fibonacci number is calculated by adding the two preceding numbers in the sequence (sequence[i - 1] and sequence[i - 2]), this is looking at the index of the array. and what ever number is at that index add it to the other. and the result is pushed into the sequence array. This sequence generation continues until the specified length is reached.
+
+
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const votes1 = { upVotes: 13, downVotes: 2 }
-// Expected output: 11
-const votes2 = { upVotes: 2, downVotes: 33 }
-// Expected output: -31
+
+describe("endTally", () => {
+  it("takes in an object that contains up votes and down votes and returns the end tally.", () => {
+    const votes1 = { upVotes: 13, downVotes: 2 }
+    // Expected output: 11
+    const votes2 = { upVotes: 2, downVotes: 33 }
+    // Expected output: -31
+    expect(endTally(votes1)).toEqual(11)
+    expect(endTally(votes2)).toEqual(-31)
+  })
+}) 
+
+//ReferenceError: endTally is not defined
 
 // b) Create the function that makes the test pass.
 
+const endTally = (votes) => {
+  return votes.upVotes - votes.downVotes
+}
+
+
 // Pseudo code:
+//created funcation. takes in a parameter of votes.
+//using dot notation to access the properties upVotes and downVotes of the votes object.
+//subtract the number of downvotes from the number of upvotes. and reurn that answer
--- ruby.rb
+++ ruby.rb
@@ -7,4 +7,47 @@
 # Add comments to your code explaining your approach
 
 class Bike
+  attr_accessor :model, :wheels, :current_speed
+  def initialize(model)
+    @model = model
+    @wheels = 2
+    @current_speed = 0
+  end
+
+  # Method that takes number as parameter and adds it to current speed which will always start with 0
+  def pedal_faster(number)
+    @current_speed += number
+  end 
+  
+  # Method that takes a number as parameter and adds to the wheels. but you cant exceed more that 3 since we only make tricycles! 
+  def tricycle(number)
+    if @wheels + number > 3
+      p "we only make tricycles!"
+    else 
+      @wheels += number
+    end
+  end
+
+  # Method that takes number as parameter and starts that if that current speed - number is less then 0 then just return 0. this way it cant be negative. if not < 0 then take that Current speed and minus the number passed as the argument.
+  def brake(number)
+    if @current_speed - number < 0
+       @current_speed = 0
+    else 
+      @current_speed -= number
+    end
+  end
+
+  # using string interpoloation prints a sentence that takes model wheelsand current speed whick are instant variables and returns whatever the data is = to.
+  def bike_info()
+    "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+  end
+
 end
+
+Bike.new('Trek')
+giant_tcr = Bike.new('Giant TCR')
+giant_tcr.pedal_faster(20)
+giant_tcr.tricycle(1)
+# giant_tcr.brake(25)
+p giant_tcr.bike_info
+p giant_tcr
